589b2d79855279ab2b9737f67f917c457365bee7
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-05 16:17:55 +0200

    Move example payload from file to code

--- test_validate_input.py
+++ test_validate_input.py
@@ -3,8 +3,39 @@ import pytest
 
 
 def test_no_error_example_input():
-    with open("example.json") as input_file:
-        input_payload = input_file.read()
+    input_payload = """
+    {
+        "address": "https://www.google.com ",
+        "content": {
+            "marks": [
+                {
+                    "text": "marks"
+                },
+                {
+                    "text": "season"
+                },
+                {
+                    "text": "foo"
+                },
+                {
+                    "text": "bar"
+                }
+            ],
+            "description": "Some description"
+        },
+        "updated": "2021-02-26T08:21:20+00:00",
+        "author": {
+            "username": "Bob",
+            "id": "68712648721648271"
+        },
+        "id": "543435435",
+        "created": "2021-02-25T16:25:21+00:00",
+        "counters": {
+            "score": 3,
+            "mistakes": 0
+        },
+        "type": "main"
+    }""".strip()
     Input.from_json(input_payload)
 
 
