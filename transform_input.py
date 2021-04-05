6329edafc204a69a2368ab66aca67876ef36dfab
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-05 23:10:45 +0200

    Add make some fields optional

--- transform_input.py
+++ transform_input.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from dataclasses_json import DataClassJsonMixin, config
-from typing import List
+from typing import List, Optional
 from datetime import date
 from datetime import time
 from pytz import timezone
@@ -11,24 +11,26 @@ from validate_input import Source
 @dataclass
 class Target((DataClassJsonMixin)):
     path: str
-    items: List[str]
-    body: str
     id: str
     author_name: str
     author_id: str
+    counters_total: int
     created_date: date = field(
         metadata=config(encoder=date.isoformat, decoder=date.fromisoformat)
     )
     created_time: time = field(
         metadata=config(encoder=time.isoformat, decoder=time.fromisoformat)
     )
-    updated_date: date = field(
-        metadata=config(encoder=date.isoformat, decoder=date.fromisoformat)
+    updated_date: Optional[date] = field(
+        metadata=config(encoder=date.isoformat, decoder=date.fromisoformat),
+        default=None,
     )
-    updated_time: time = field(
-        metadata=config(encoder=time.isoformat, decoder=time.fromisoformat)
+    updated_time: Optional[time] = field(
+        metadata=config(encoder=time.isoformat, decoder=time.fromisoformat),
+        default=None,
     )
-    counters_total: int
+    items: Optional[List[str]] = None
+    body: Optional[str] = None
 
     def __init__(self, source: Source):
         self.path = source.address
