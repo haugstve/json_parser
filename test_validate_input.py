f48a91b89f644fc13bc1c1efd48cbb9839ecdc1e
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-05 16:22:01 +0200

    Add test of date format

--- test_validate_input.py
+++ test_validate_input.py
@@ -79,3 +79,41 @@ def test_error_when_missing_address():
     """.strip()
     with pytest.raises(KeyError):
         Input.from_json(input_payload)
+
+
+def test_error_when_dateformat_is_wrong():
+    input_payload = """
+    {
+        "address": "https://www.google.com ",
+        "content": {
+            "marks": [
+                {
+                    "text": "marks"
+                },
+                {
+                    "text": "season"
+                },
+                {
+                    "text": "foo"
+                },
+                {
+                    "text": "bar"
+                }
+            ],
+            "description": "Some description"
+        },
+        "updated": "2021/02/26T08:21:20+00:00",
+        "author": {
+            "username": "Bob",
+            "id": "68712648721648271"
+        },
+        "id": "543435435",
+        "created": "2021-02-25T16:25:21+00:00",
+        "counters": {
+            "score": 3,
+            "mistakes": 0
+        },
+        "type": "main"
+    }""".strip()
+    with pytest.raises(ValueError):
+        Input.from_json(input_payload)
