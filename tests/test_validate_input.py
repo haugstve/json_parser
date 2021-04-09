48acef69bf237cdc1c6c353f5f2d3f1b0417ef87
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-09 17:24:24 +0200

    Add test of no description

--- tests/test_validate_input.py
+++ tests/test_validate_input.py
@@ -218,4 +218,32 @@ def test_error_when_content_has_no_mark():
         Source.parse_raw(input_payload)
 
 
-test_no_error_missing_updated()
+def test_error_when_content_has_no_description():
+    input_payload = """
+    {
+        "address": "https://www.google.com ",
+        "content": {
+            "marks": [
+                {
+                    "text": "marks"
+                },
+                {
+                    "text": "season"
+                }
+            ]
+        },
+        "updated": "2021-02-26T08:21:20+00:00",
+        "author": {
+            "username": "Bob",
+            "id": "68712648721648271"
+        },
+        "id": "543435435",
+        "created": "2021-02-25T16:25:21+00:00",
+        "counters": {
+            "score": 3,
+            "mistakes": 0
+        },
+        "type": "main"
+    }""".strip()
+    with pytest.raises(ValidationError):
+        Source.parse_raw(input_payload)
