54b3507af455cf3ebfe2a935cafd56d883e30e85
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-09 17:17:34 +0200

    Remove comments

--- app/connect.py
+++ app/connect.py
@@ -77,30 +77,3 @@ class DataBase:
         print(db_version)
 
         cur.close()
-
-    # conn.autocommit = True
-
-
-# def test_connect():
-#     """ Connect to the PostgreSQL database server """
-#     conn = None
-#     try:
-#         params = config()
-
-#         print("Connecting to the PostgreSQL database...")
-#         conn = psycopg2.connect(**params)
-#         cur = conn.cursor()
-
-#         print("PostgreSQL database version:")
-#         cur.execute("SELECT version()")
-
-#         db_version = cur.fetchone()
-#         print(db_version)
-
-#         cur.close()
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print("Database connection closed.")
