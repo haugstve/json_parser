b7eacab39140996b6faa76ad329d80cde990d5c3
Daniel Haugstvedt <daniel.haugstvedt@gmail.com>
2021-04-09 17:13:51 +0200

    Remove dead code and unused features

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI, Depends
-import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 from validate_input import Source
 from transform_input import Target
 from connect import DataBase
 
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
-
 db = DataBase("target")
-
-db.test_connection()
 
 
 @app.post("/transform")
@@ -26,12 +22,6 @@ def validate_json(input: Source):
     return JSONResponse(content=jsonable_encoder(input))
 
 
-@app.get("/test_connection")
-def connect_to_db():
-    print(type(db))
-    db.test_connection()
-
-
 @app.post("/store")
 def store_to_db(input: Source):
     target = Target(input)
@@ -41,7 +31,3 @@ def store_to_db(input: Source):
 @app.get("/retrive")
 def retrive_all():
     return db.read_all_entries()
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
